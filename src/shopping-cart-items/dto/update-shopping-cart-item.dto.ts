aba3c13ed724971407e048e92a114392d154a9f2
Gilad Cypel <giladc@gmail.com>
2025-04-26 19:15:12 +0300

    Delete dto and entity files

--- src/shopping-cart-items/dto/update-shopping-cart-item.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateShoppingCartItemDto } from './create-shopping-cart-item.dto';
-
-export class UpdateShoppingCartItemDto extends PartialType(CreateShoppingCartItemDto) {}
